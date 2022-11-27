04d7d52c6b2a5b0ed681f33d660b7ccb5bb2f93d
audrNa <dddsssoff@gmail.com>
2022-11-27 21:11:37 +0800

    Remove url capitalizer

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const links = [
+const LINKS = [
     {
         "title": "Home",
         "url":   "."
@@ -15,17 +15,17 @@ const links = [
     },
 ];
 
-const footer          = "If there are incorrect, missing, or outdated info/links, please contact audr.";
-const icon            = './static/favicon.ico';
-const headerImg       = './static/prayge.png';
-const repositoryURL   = 'https://github.com/10Genitals/lmsppp/';
-var lastCommitBadge   = 'https://badgen.net/github/last-commit/10genitals/lmsppp/';
+const FOOTER         = "If there are incorrect, missing, or outdated info/links, please contact audr.";
+const ICON_URL       = './static/favicon.ico';
+const HEADER_IMG     = './static/prayge.png';
+const REPOSITORY_URL = 'https://github.com/10Genitals/lmsppp/';
+const LCB_URL        = 'https://badgen.net/github/last-commit/10genitals/lmsppp/';
 
 // Executed when page loads
 document.addEventListener('DOMContentLoaded', () => {
     const body = document.body;
     body.prepend(createNavBar());
-    body.prepend(createLCBadge());
+    body.prepend(createLCB());
     body.prepend(createHeader());
     body.append(createFooter());
     setIcon();
@@ -40,7 +40,7 @@ function createHeader()
 
     // Create image
     const img = document.createElement('img');
-    img.src = headerImg;
+    img.src = HEADER_IMG;
     img.style.width = '64px';
     img.style.height = '64px';
 
@@ -54,7 +54,7 @@ function createNavBar()
 {
     const navBar = document.createElement('div');
     navBar.id = 'navbar';
-    for (const link of links)
+    for (const link of LINKS)
     {
         const navLink = createNavLink(link);
         navBar.appendChild(navLink);
@@ -77,7 +77,7 @@ function createNavLink(link)
 function createFooter()
 {
     const div = document.createElement('div');
-    div.innerHTML = footer;
+    div.innerHTML = FOOTER;
     div.classList.add('center')
     div.id = 'footer'
 
@@ -85,34 +85,14 @@ function createFooter()
 }
 
 // Creates an iframe to display last commit badge of this repository
-function createLCBadge()
+function createLCB()
 {
     const img = document.createElement('img')
-
-    // We capitalize letters in the badge url randomly to force badgen.net to 
-    // generate a new badge nearly every time someone opens this website,
-    // because it takes a while for them to generate a new badge so the badge
-    // will be stuck and outdated. Though it's only useful when it's only
-    // been minutes since the last commit, so it might not be worth it.
-    // i sure do hope they dont ban my repository for this
-    const min = 40;
-    const max = 55;
-    const amount = rng(1, max - min + 1);
-
-    // Capitalize random letters in the owner and repo part
-    for (let i = 0; i < amount; i++)
-    {
-        const index = rng(min, max);
-        lastCommitBadge = lastCommitBadge.slice(0, index) + lastCommitBadge[index].toUpperCase() + lastCommitBadge.slice(index + 1);
-        // why the fk is javascript like this
-        // why couldnt i just lastCommitBadge[index] = lastCommitBadge[index].toUpperCase()
-        // or even lastCommitBadge[index].toUpperCase()
-    }
-    img.src = lastCommitBadge;
+    img.src = LCB_URL;
 
     const anchor = document.createElement('a');
     anchor.appendChild(img);
-    anchor.href = repositoryURL + 'commits/master/';
+    anchor.href = REPOSITORY_URL + 'commits/master/';
 
     const div = document.createElement('div');
     div.appendChild(anchor);
@@ -136,7 +116,7 @@ function setIcon()
     }
 
     // Set favicon
-    link.href = icon;
+    link.href = ICON_URL;
 }
 
 // Returns random number between min and max inclusive
